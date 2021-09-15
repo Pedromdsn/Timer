4a455560ff7caeaeb136833039ef5d97cfeab350
Pedromdsn <pedromdsn.2002@hotmail.com>
2021-09-16 00:24:42 +0100

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
-import 'tailwindcss/tailwind.css'
-import 'react-toastify/dist/ReactToastify.css';
+import Head from "next/head"
+
+import "tailwindcss/tailwind.css"
+import "react-toastify/dist/ReactToastify.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+        <link rel="icon" type="image/ico" href="/coco.ico"/>
+				<title>Temporizador</title>
+				<meta name="description" content="O melhor temporizador nda web" />
+				<meta name="author" content="Coco Blanco" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
