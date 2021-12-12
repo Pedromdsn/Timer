ee0fc64913afaacd2a9a33ee9abecb44d6a862bf
Pedromdsn <pedromdsn.2002@hotmail.com>
2021-12-12 00:53:51 +0000

    fit: bug fix

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,6 @@
 export const getTime = (timeDif: number) => {
 	return {
-		dias: Math.floor(timeDif % (60 * 60 * 24) / (60 * 60 * 24)),
+		dias: Math.floor(timeDif / (60 * 60 * 24)),
 		horas: Math.floor((timeDif % (60 * 60 * 24)) / (60 * 60)),
 		minutos: Math.floor((timeDif % (60 * 60)) / 60),
 		segundos: Math.floor(timeDif % 60),
